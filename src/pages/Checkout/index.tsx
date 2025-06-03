3fdd84816420f6efb3766aa5ad30f96699fa600b
jonesnambunndo@hotmail.com <jonesnambundo@mtel.inatel.br>
2025-06-02 22:22:36 -0300

    atualização

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,11 +1,12 @@
 import { CreditCard, Barcode } from "lucide-react";
 import { useState } from "react";
 import { useFormik } from "formik";
+
 import * as Yup from "yup";
 
 export default function CheckoutCards() {
   const [payWithCard, setPayWithCard] = useState(false);
-  const [success, setSuccess] = useState(false); 
+  const [success, setSuccess] = useState(false);
 
   const form = useFormik({
     initialValues: {
@@ -31,16 +32,15 @@ export default function CheckoutCards() {
         .email("Invalid email address")
         .required("This field is required"),
       cpf: Yup.string()
-        .min(14, "O campo precisa ter 14 caracteres")
-        .max(15, "O campo precisa ter 14 caracteres")
-        .required("O campo é obrigatório"),
+        .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "Invalid CPF (format: 000.000.000-00)")
+        .required("CPF is required"),
       deliveryEmail: Yup.string()
         .email("Invalid email address")
         .required("This field is required"),
       confirmDeliveryEmail: Yup.string()
         .oneOf([Yup.ref("deliveryEmail")], "Emails must match")
         .required("This field is required"),
-      cardOwner: Yup.string().when([], { // corrigido para avaliar o estado payWithCard externo
+      cardOwner: Yup.string().when([], {
         is: () => payWithCard,
         then: (schema) =>
           schema.min(5, "Name must be at least 5 characters long").required("This field is required"),
@@ -67,22 +67,23 @@ export default function CheckoutCards() {
         then: (schema) => schema.required("This field is required"),
         otherwise: (schema) => schema.notRequired(),
       }),
+
       expiresYear: Yup.string().when([], {
         is: () => payWithCard,
         then: (schema) => schema.required("This field is required"),
         otherwise: (schema) => schema.notRequired(),
       }),
+
       cardCode: Yup.string().when([], {
         is: () => payWithCard,
         then: (schema) => schema.required("This field is required"),
         otherwise: (schema) => schema.notRequired(),
       }),
     }),
+
     onSubmit: (values) => {
-      // aqui assumimos que validação passou
       setSuccess(true);
       console.log("Form data:", values);
-      // Você pode adicionar chamadas API aqui se quiser
     },
   });
 
@@ -93,84 +94,77 @@ export default function CheckoutCards() {
   };
 
   if (success) {
-    // Mensagem após sucesso no submit e validação completa
     return (
-      <div className="max-w-[1024px] mx-auto px-4 py-8 text-white bg-zinc-900 rounded-lg">
-        <h2 className="text-2xl font-bold mb-4">Muito obrigado</h2>
+      <div className="max-w-[1024px] mx-auto px-12 py-8 text-white bg-zinc-900 rounded-lg mt-16">
+        <h2 className="text-2xl font-bold mb-4">Thank you very much</h2>
         <p>
-          É com satisfação que informamos que recebemos seu pedido com sucesso!
+          We are pleased to inform you that we have successfully received your order!
         </p>
         <p className="mt-4 font-semibold">
-          Abaixo estão os detalhes da sua compra:
+          Below are the details of your purchase:
         </p>
         <ul className="list-disc list-inside mb-4">
           <li>
-            Número do pedido: <strong>#1004404</strong>
+            Order number: <strong>#1004404</strong>
           </li>
           <li>
-            Forma de pagamento:{" "}
+            Payment method:{" "}
             <strong>
-              {payWithCard ? "Cartão de Crédito" : "Boleto Bancário"}
+              {payWithCard ? "Credit Card" : "Bank Slip"}
             </strong>
           </li>
         </ul>
         {!payWithCard && (
           <p>
-            Caso tenha optado pelo pagamento via boleto bancário, lembre-se de
-            que a confirmação pode levar até 3 dias úteis. Após a aprovação do
-            pagamento, enviaremos um e-mail contendo o código de ativação do
-            jogo.
+            If you chose to pay by bank slip, please note that confirmation may take up to 3 business days. After payment approval, we will send you an email containing the activation code for the game.
           </p>
         )}
         {payWithCard && (
           <p>
-            Se você optou pelo pagamento com cartão de crédito, a liberação do
-            código de ativação ocorrerá após a aprovação da transação pela
-            operadora do cartão. Você receberá o código no e-mail cadastrado em
-            nossa loja.
+            If you chose to pay by credit card, the activation code will be released after the transaction is approved by the card operator. You will receive the code at the email registered in our store.
           </p>
         )}
         <p className="mt-4">
-          Pedimos que verifique sua caixa de entrada e a pasta de spam para
-          garantir que receba nossa comunicação. Caso tenha alguma dúvida ou
-          necessite de mais informações, por favor, entre em contato conosco
-          através dos nossos canais de atendimento ao cliente.
+          Please check your inbox and spam folder to ensure you receive our communication. If you have any questions or need more information, please contact us through our customer service channels.
         </p>
         <p className="mt-6 font-semibold">
-          Agradecemos por escolher a Suri e esperamos que gosto de sua compra!
+          Thank you for choosing Suri. We hope you enjoy your purchase!
         </p>
       </div>
     );
   }
 
-  // Formulário exibido enquanto não houve sucesso no submit
   return (
     <form
       onSubmit={form.handleSubmit}
       className="max-w-[1024px] mx-auto px-4 py-8 space-y-8 text-white"
     >
-      {/* Dados de cobrança */}
-      <div className="bg-zinc-900 rounded-lg p-6">
-        <h2 className="text-xl font-semibold mb-6">Dados de cobrança</h2>
+      {/* Billing Information */}
+      <div className="bg-white rounded-lg p-6 shadow-lg">
+        <h2 className="text-xl font-semibold mb-6 text-black">
+          Billing Information
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
           <div>
             <input
               id="fullName"
               name="fullName"
               type="text"
-              placeholder="Nome completo"
-              className="w-full px-4 py-3 rounded bg-zinc-800 text-white focus:outline-none"
+              placeholder="Full Name"
+              className="w-full px-4 py-3 rounded bg-zinc-200 text-black focus:outline-none"
               {...form.getFieldProps("fullName")}
             />
-            <small className="text-red-500">{getErrorMessage("fullName")}</small>
+            <small className="text-red-500">
+              {getErrorMessage("fullName")}
+            </small>
           </div>
           <div>
             <input
               id="email"
               name="email"
               type="email"
-              placeholder="E-mail"
-              className="w-full px-4 py-3 rounded bg-zinc-800 text-white focus:outline-none"
+              placeholder="Email"
+              className="w-full px-4 py-3 rounded bg-zinc-200 text-black focus:outline-none"
               {...form.getFieldProps("email")}
             />
             <small className="text-red-500">{getErrorMessage("email")}</small>
@@ -181,14 +175,14 @@ export default function CheckoutCards() {
               name="cpf"
               type="text"
               placeholder="CPF"
-              className="w-full px-4 py-3 rounded bg-zinc-800 text-white focus:outline-none"
+              className="w-full px-4 py-3 rounded bg-zinc-200 text-black focus:outline-none"
               {...form.getFieldProps("cpf")}
             />
             <small className="text-red-500">{getErrorMessage("cpf")}</small>
           </div>
         </div>
-        <h3 className="text-lg font-semibold mb-4">
-          Dados de entrega - conteúdo digital
+        <h3 className="text-lg font-semibold mb-4 text-black">
+          Delivery Information - Digital Content
         </h3>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
@@ -196,29 +190,33 @@ export default function CheckoutCards() {
               id="deliveryEmail"
               name="deliveryEmail"
               type="email"
-              placeholder="E-mail"
-              className="w-full px-4 py-3 rounded bg-zinc-800 text-white focus:outline-none"
+              placeholder="Email"
+              className="w-full px-4 py-3 rounded bg-zinc-200 text-black focus:outline-none"
               {...form.getFieldProps("deliveryEmail")}
             />
-            <small className="text-red-500">{getErrorMessage("deliveryEmail")}</small>
+            <small className="text-red-500">
+              {getErrorMessage("deliveryEmail")}
+            </small>
           </div>
           <div>
             <input
               id="confirmDeliveryEmail"
               name="confirmDeliveryEmail"
               type="email"
-              placeholder="Confirme o e-mail"
-              className="w-full px-4 py-3 rounded bg-zinc-800 text-white focus:outline-none"
+              placeholder="Confirm Email"
+              className="w-full px-4 py-3 rounded bg-zinc-200 text-black focus:outline-none"
               {...form.getFieldProps("confirmDeliveryEmail")}
             />
-            <small className="text-red-500">{getErrorMessage("confirmDeliveryEmail")}</small>
+            <small className="text-red-500">
+              {getErrorMessage("confirmDeliveryEmail")}
+            </small>
           </div>
         </div>
       </div>
 
-      {/* Pagamento */}
-      <div className="bg-zinc-900 rounded-lg p-6">
-        <h2 className="text-xl font-semibold mb-6">Pagamento</h2>
+      {/* Payment */}
+      <div className="bg-white shadow-lg rounded-lg p-6">
+        <h2 className="text-xl font-semibold mb-6 text-black">Payment</h2>
         <div className="flex gap-4 mb-4">
           <button
             type="button"
@@ -228,7 +226,7 @@ export default function CheckoutCards() {
             } text-gray-300 hover:bg-zinc-700`}
           >
             <Barcode className="w-5 h-5" />
-            Boleto bancário
+            Bank Slip
           </button>
           <button
             type="button"
@@ -238,40 +236,45 @@ export default function CheckoutCards() {
             } text-gray-300 hover:bg-zinc-700`}
           >
             <CreditCard className="w-5 h-5" />
-            Cartão de crédito
+            Credit Card
           </button>
         </div>
 
         {!payWithCard && (
-          <p className="text-sm text-gray-300">
-            Ao optar por essa forma de pagamento, a confirmação pode levar até 3 dias úteis. A liberação do código de ativação ocorrerá após a aprovação.
+          <p className="text-sm text-black">
+            By choosing this payment method, confirmation may take up to 3
+            business days. Activation code will be released after approval.
           </p>
         )}
 
         {payWithCard && (
-          <div className="bg-zinc-900 rounded-lg p-6 text-black">
+          <div className="bg-white shadow-lg rounded-lg p-6 text-black">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
               <div>
                 <input
                   id="cardOwner"
                   name="cardOwner"
                   type="text"
-                  placeholder="Nome do titular do cartão"
-                  className="w-full px-4 py-3 rounded bg-white focus:outline-none"
+                  placeholder="Cardholder Name"
+                  className="w-full px-4 py-3 rounded bg-zinc-200 focus:outline-none"
                   {...form.getFieldProps("cardOwner")}
                 />
-                <small className="text-red-500">{getErrorMessage("cardOwner")}</small>
+                <small className="text-red-500">
+                  {getErrorMessage("cardOwner")}
+                </small>
               </div>
               <div>
                 <input
                   id="cpfCardOwner"
                   name="cpfCardOwner"
                   type="text"
-                  placeholder="CPF do titular do cartão"
-                  className="w-full px-4 py-3 rounded bg-white focus:outline-none"
+                  placeholder="Cardholder CPF"
+                  className="w-full px-4 py-3 rounded bg-zinc-200 focus:outline-none"
                   {...form.getFieldProps("cpfCardOwner")}
                 />
-                <small className="text-red-500">{getErrorMessage("cpfCardOwner")}</small>
+                <small className="text-red-500">
+                  {getErrorMessage("cpfCardOwner")}
+                </small>
               </div>
             </div>
 
@@ -281,22 +284,26 @@ export default function CheckoutCards() {
                   id="cardDisplayName"
                   name="cardDisplayName"
                   type="text"
-                  placeholder="Nome no cartão"
-                  className="w-full px-4 py-3 rounded bg-white focus:outline-none"
+                  placeholder="Name on Card"
+                  className="w-full px-4 py-3 rounded bg-zinc-200 focus:outline-none"
                   {...form.getFieldProps("cardDisplayName")}
                 />
-                <small className="text-red-500">{getErrorMessage("cardDisplayName")}</small>
+                <small className="text-red-500">
+                  {getErrorMessage("cardDisplayName")}
+                </small>
               </div>
               <div>
                 <input
                   id="cardNumber"
                   name="cardNumber"
                   type="text"
-                  placeholder="Número do cartão"
-                  className="w-full px-4 py-3 rounded bg-white focus:outline-none"
+                  placeholder="Card Number"
+                  className="w-full px-4 py-3 rounded bg-zinc-200 focus:outline-none"
                   {...form.getFieldProps("cardNumber")}
                 />
-                <small className="text-red-500">{getErrorMessage("cardNumber")}</small>
+                <small className="text-red-500">
+                  {getErrorMessage("cardNumber")}
+                </small>
               </div>
             </div>
 
@@ -306,24 +313,28 @@ export default function CheckoutCards() {
                   id="expiresMonth"
                   name="expiresMonth"
                   type="text"
-                  placeholder="Mês de expiração"
+                  placeholder="Expiration Month"
                   maxLength={2}
-                  className="w-full px-4 py-3 rounded bg-white focus:outline-none"
+                  className="w-full px-4 py-3 rounded bg-zinc-200 focus:outline-none"
                   {...form.getFieldProps("expiresMonth")}
                 />
-                <small className="text-red-500">{getErrorMessage("expiresMonth")}</small>
+                <small className="text-red-500">
+                  {getErrorMessage("expiresMonth")}
+                </small>
               </div>
               <div>
                 <input
                   id="expiresYear"
                   name="expiresYear"
                   type="text"
-                  placeholder="Ano de expiração"
+                  placeholder="Expiration Year"
                   maxLength={4}
-                  className="w-full px-4 py-3 rounded bg-white focus:outline-none"
+                  className="w-full px-4 py-3 rounded bg-zinc-200 focus:outline-none"
                   {...form.getFieldProps("expiresYear")}
                 />
-                <small className="text-red-500">{getErrorMessage("expiresYear")}</small>
+                <small className="text-red-500">
+                  {getErrorMessage("expiresYear")}
+                </small>
               </div>
               <div>
                 <input
@@ -332,35 +343,39 @@ export default function CheckoutCards() {
                   type="text"
                   placeholder="CVV"
                   maxLength={4}
-                  className="w-full px-4 py-3 rounded bg-white focus:outline-none"
+                  className="w-full px-4 py-3 rounded bg-zinc-200 focus:outline-none"
                   {...form.getFieldProps("cardCode")}
                 />
-                <small className="text-red-500">{getErrorMessage("cardCode")}</small>
+                <small className="text-red-500">
+                  {getErrorMessage("cardCode")}
+                </small>
               </div>
             </div>
 
             <div>
-              <label className="block mb-1 font-semibold text-white">Parcelamento</label>
+              <label className="block mb-1 font-semibold text-black">
+                Installments
+              </label>
               <select
                 name="installments"
-                className="w-40 px-4 py-3 rounded bg-white text-black focus:outline-none"
+                className="w-40 px-4 py-3 rounded bg-zinc-200 text-black focus:outline-none"
                 {...form.getFieldProps("installments")}
               >
-                <option value="1">1x de R$ 169,90</option>
-                <option value="2">2x de R$ 85,00</option>
-                <option value="3">3x de R$ 56,63</option>
+                <option value="1">1x of $169.90</option>
+                <option value="2">2x of $85.00</option>
+                <option value="3">3x of $56.63</option>
               </select>
             </div>
           </div>
         )}
-      </div>
 
-      <button
-        type="submit"
-        className="bg-green-600 hover:bg-green-700 text-white font-semibold py-3 px-6 rounded"
-      >
-        Finalizar Compra
-      </button>
+        <button
+          type="submit"
+          className="w-full my-2 bg-green-600 hover:bg-green-700 text-white font-semibold py-3 px-6 rounded"
+        >
+          Complete Purchase
+        </button>
+      </div>
     </form>
   );
 }
